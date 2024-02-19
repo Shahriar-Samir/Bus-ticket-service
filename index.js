a49695c86883da401e8e4b93f03862dad44ff33b
Shahriar-Samir <shabusiness035@gmail.com>
2024-02-19 12:56:35 +0600

    corrected ui design and functionality

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+// disabled buttons
+couponApply.setAttribute('disabled',true)
+nextBtn.setAttribute('disabled', true)
+
+// html elements
 const seats = document.getElementsByClassName('seat')
 const totalSeats = document.getElementById('seats')
 const seatsRemaining = document.getElementById('seats-remaining')  
@@ -5,18 +10,54 @@ const selectedSeats = document.getElementById('selected-seats')
 const totalPrice = document.getElementById('total-price')
 const grandTotal = document.getElementById('grand-total')
 const nextBtn = document.getElementById('next')
-nextBtn.setAttribute('disabled', true)
 const form = document.getElementById('form')
 const phone = document.getElementById('phone')
 const main = document.getElementById('main')
 const modal = document.getElementById('modal')
 const couponCode = document.getElementById('coupon-code')
 const couponApply = document.getElementById('coupon-apply')
-couponApply.setAttribute('disabled',true)
 const couponField = document.getElementById("coupon-field")
 const discountEl = document.getElementById('discount')
+const continueBtn = document.getElementById('continue')
+
+// discount amount
 let discount = 0 
 
+// functions 
+
+//  coupon apply button enabling function
+function enableCouponApplyBtn(){
+    if(parseFloat(totalSeats.innerText) > 3 ){
+        couponApply.removeAttribute('disabled')
+        couponApply.classList.remove('bg-[#1cd10060]')
+        couponApply.classList.add('bg-[#1DD100]')
+    }
+    else{
+        discountEl.classList.add('hidden')
+        couponField.classList.remove('hidden')
+        couponApply.setAttribute('disabled', true)
+        couponApply.classList.remove('bg-[#1DD100]')
+        couponApply.classList.add('bg-[#1cd10060]')
+    }
+}
+
+// next button enabling function
+function enableNextBtn(){
+    if(parseFloat(totalSeats.innerText) > 0 && phone.value.length > 0){
+        nextBtn.removeAttribute('disabled')
+        nextBtn.classList.remove('bg-[#1cd10060]')
+        nextBtn.classList.add('bg-[#1DD100]')
+
+    }
+       else{
+        nextBtn.setAttribute('disabled', true)
+        nextBtn.classList.remove('bg-[#1DD100]')
+        nextBtn.classList.add('bg-[#1cd10060]')
+    }
+}
+
+
+// coupon apply event and functionality
 couponApply.addEventListener('click', function(){
      
         if(couponCode.value === 'NEW15'){
@@ -40,6 +81,7 @@ couponApply.addEventListener('click', function(){
         }
 })
 
+// form submit event and functionality
 form.addEventListener('submit',(e)=>{
         e.preventDefault()
         main.classList.add('hidden')
@@ -48,8 +90,9 @@ form.addEventListener('submit',(e)=>{
 })
 
 
+//  for each seat button's event and functionality
 for(const seat of seats){
-    seat.addEventListener('click',(e)=>{  
+            seat.addEventListener('click',(e)=>{  
            if(seat.classList.contains('selected')){
             grandTotal.innerText = parseFloat(grandTotal.innerText)+discount
             discount = 0
@@ -73,7 +116,7 @@ for(const seat of seats){
               grandTotal.innerText = parseFloat(grandTotal.innerText)+550
            }
            else{
-            alert('no more')
+            alert("You can't select more than 4 seats")
            }
             enableNextBtn()
             enableCouponApplyBtn()
@@ -82,13 +125,14 @@ for(const seat of seats){
 }
 
 
+// phone input field's event and functionality
 phone.addEventListener('keyup', function(){
         enableNextBtn()
 })
 
 
-const continueBtn = document.getElementById('continue')
 
+// modal continue button's event and functionality
 continueBtn.addEventListener('click', ()=>{
         discount = 0
         discountEl.classList.add('hidden')
@@ -101,7 +145,11 @@ continueBtn.addEventListener('click', ()=>{
         modal.classList.remove('flex')
         modal.classList.add('hidden')
         for(const seat of seats){
+            if(seat.classList.contains('selected')===true){
             seat.classList.remove('selected')
+            seat.classList.add('sold')
+            seat.setAttribute('disabled',true)
+            }
         }
         selectedSeats.innerHTML = ''
         enableCouponApplyBtn()
@@ -109,32 +157,3 @@ continueBtn.addEventListener('click', ()=>{
 
 })
 
-// functions 
-function enableCouponApplyBtn(){
-    if(parseFloat(totalSeats.innerText) > 3 ){
-        couponApply.removeAttribute('disabled')
-        couponApply.classList.remove('bg-[#1cd10060]')
-        couponApply.classList.add('bg-[#1DD100]')
-    }
-       else{
-        discountEl.classList.add('hidden')
-        couponField.classList.remove('hidden')
-        couponApply.setAttribute('disabled', true)
-        couponApply.classList.remove('bg-[#1DD100]')
-        couponApply.classList.add('bg-[#1cd10060]')
-    }
-}
-
-function enableNextBtn(){
-    if(parseFloat(totalSeats.innerText) > 0 && phone.value.length > 0){
-        nextBtn.removeAttribute('disabled')
-        nextBtn.classList.remove('bg-[#1cd10060]')
-        nextBtn.classList.add('bg-[#1DD100]')
-
-    }
-       else{
-        nextBtn.setAttribute('disabled', true)
-        nextBtn.classList.remove('bg-[#1DD100]')
-        nextBtn.classList.add('bg-[#1cd10060]')
-    }
-}
